a75195a960ac78a77a78a17aecccc824d4710b4c
KeganF <keganferdinand@gmail.com>
2023-03-07 11:30:02 -0500

    added error handling for invalid cities

--- README.md
+++ README.md
@@ -16,6 +16,14 @@ You can check out OpenWeather [here](https://openweathermap.org/) to sign up for
 
 ## Updates
 
+- March 7th, 2023: First solution for error handling. After a bit of research and some experimenting, I implemented a way to prevent invalid city names from breaking the program. The section of code that normally grabs and displays the weather data now throws an error and redirects users to an error page when an invalid city name is entered. I'm still keeping an eye out for what's considered best practice for this situation, and will continue to update my solution as I go.
+
+![](public/screenshots/screenshot3-home.png)
+
+![](public/screenshots/screenshot4-weather.png)
+
+![](public/screenshots/screenshot5-error.png)
+
 - March 1st, 2023: First test implementing a search bar. This is my first time attempting to create a search bar feature, so there's some research I need to do on error handling before I continue working on the project so that invalid city names don't cause issues.
 
 ![](public/screenshots/screenshot2.png)
--- weather.js
+++ weather.js
@@ -12,56 +12,65 @@ weatherApp.set("view engine", "ejs");
 // App configuration
 weatherApp.use(express.static(path.join(__dirname, "public")));
 
+
+var cityString;
+
 weatherApp.get('/', function(req, res) {
-    // API key removed for privacy reasons. 
-    // You can receive and use a free API key from https://openweathermap.org/
-    // To use the weather app, you can set your own definition 
-    // using your API key in this format:
-    // 
-    // const apiKey = "yourapikey";
-    const apiKey = "";
-
-    // Also for privacy, the cityNameString is left blank for now.
-    // A default value will need to be set for the app to work.
-    var cityNameString = "";
+    res.sendFile(path.join(__dirname, "views/index.html"));
+    
     if (req.query.city != undefined) {
-        cityNameString = req.query.city;
-        cityNameString = formatQueryStringToCityName(cityNameString);
+        cityString = formatQueryStringToCityName(req.query.city);
+        res.redirect('/current-weather');
     }
+});
 
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityNameString + "&units=metric&appid=" + apiKey;
+weatherApp.get('/current-weather', function(req, res) {
+    // You can sign up for a free API key here: https://openweathermap.org/
+    const apiKey = "yourapikeyhere";
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityString + "&units=metric&appid=" + apiKey;
     
     request(url, function(err, response, body) {
         if (err) {
             console.log("Error: " + err);
         }
         else {
-            var weather = JSON.parse(body);
+            try {
+                var weather = JSON.parse(body);
 
-            var iconCode = weather.weather[0].icon;
-            var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-            
-            res.render("index", { 
-                title: "Weather", 
-                cityName: "City Name",
-                wicon: iconUrl,
-                temp: parseInt(weather.main.temp),
-                feelsLike: parseInt(weather.main.feels_like),
-                conditions: weather.weather[0].description,
-                windSpeed: weather.wind.speed,
-                pressure: weather.main.pressure / 10,
-                humidity: weather.main.humidity
-            });
-
-            console.log(`Weather data for ${weather.name} has been rendered to Index`);
+                var iconCode = weather.weather[0].icon;
+                var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+                
+                res.render("currentWeather", { 
+                    title: "Weather", 
+                    cityName: weather.name,
+                    wicon: iconUrl,
+                    temp: parseInt(weather.main.temp),
+                    feelsLike: parseInt(weather.main.feels_like),
+                    conditions: weather.weather[0].description,
+                    windSpeed: weather.wind.speed,
+                    pressure: weather.main.pressure / 10,
+                    humidity: weather.main.humidity
+                });
+    
+                console.log(`Weather data for ${weather.name} has been rendered to currentWeather.ejs`);
+            }
+            catch {
+                console.log(`${cityString} was an invalid input, redirecting to error page.`);
+                res.redirect('/error');
+            }
         }
     });
+});
+
+weatherApp.get('/error', function(req, res) {
+    res.sendFile(path.join(__dirname, "views/error.html"));
 });
 
 // Create server
 weatherApp.listen(port, () => console.log(`Weather App is running on port ${port}.`));
 
 
+// Converts query string to appropriate city name for OpenWeather API to use
 function formatQueryStringToCityName(search) {
     search = search.toLowerCase();
     search = search.replace(" ", "+");
